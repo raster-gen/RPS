4215d54fb11c1d746170c1af02cd527f28bde720
raster-gen <rasterroot@gmail.com>
2022-11-05 14:42:46 +0000

    create base ui without any functionality

--- RPS/ContentView.swift
+++ RPS/ContentView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var gameItems = ["Rock", "Paper", "Scissors"]
+    
+    private var aiChoise: String {
+        get{
+            gameItems.shuffled()[0]
+        }
+    }
+    
+    @State private var winOrLose = ["Win", "Lose"].shuffled()
+    
+    @State private var playerChoise = "Rock"
+    
+    @State private var showAnswer = false
+    
+    @State private var round = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack{
+                VStack{
+                    // AI select
+                    Section{
+                        HStack{
+                            Text(aiChoise)
+                            Text(winOrLose[0])
+                        }
+                    } header: {
+                        Text("AI need:")
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    // Player select
+                    Section{
+                        VStack{
+                            
+                        }
+                    } header: {
+                        Text("Your choise:")
+                        
+                    }
+                    Picker("Player Choise", selection: $playerChoise){
+                        ForEach(gameItems, id: \.self){ item in
+                            Text(item)
+                            
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    Button{
+                        showAnswer = true
+                    } label: {
+                        Label("Submit", systemImage: "checkmark")
+                            
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
+            .navigationTitle("Rock.Paper.Scissors")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
