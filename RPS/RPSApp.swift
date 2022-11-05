00290b059ee23569d321898da7c53bd67967454c
raster-gen <rasterroot@gmail.com>
2022-11-05 13:19:00 +0000

    initial commit

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by Gennady Raster on 5.11.22.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
